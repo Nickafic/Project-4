b6c721bed87c4f68cad5e0bda7bba4c0d351b63b
Nickafic <85850258+Nickafic@users.noreply.github.com>
2021-11-08 23:30:24 -0800

    catch exceptions

--- heapTest.java
+++ heapTest.java
@@ -1,7 +1,14 @@
+
 import java.io.*;
 public class heapTest {
     public static void main(String[] args) throws IOException{
-        MaxHeap<String> testHeap = new MaxHeap<>();
-        testHeap.optimalMethod("data_sorted.txt");
+        try{
+            MaxHeap<String> testHeap = new MaxHeap<>(105);
+            testHeap.optimalMethod("data_sorted.txt");
+        }
+        catch(IOException e){
+            System.out.println("Error While Reading");
+        }
+
     }
 }
